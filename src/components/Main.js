a8d81efe1a393de9d9fc6a1680768ded5dd9d260
Archana Alagesan <archanaalagesan@gmail.com>
2026-03-21 18:37:49 +0530

    Initial HTML Layout

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <main>
+      <h2>Welcome to Little Lemon</h2>
+      <p>Your one-stop destination for delicious food and great ambiance.</p>
+    </main>
+  );
+}
+
+export default Main;
